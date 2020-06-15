a99671c44c6cbf22df7b8bfa42a1dfdee543435e
Manisha Priyadarshini <manishapriya94@github.com>
2020-06-15 13:03:19 -0700

    remove problem causing script

--- ally-guide/src/views/Educate.vue
+++ ally-guide/src/views/Educate.vue
@@ -153,32 +153,4 @@ What is Juneeteenth?     <i class="fa fa-plus"></i>
 </body>
 
 </template>
-<script>
-$(document).ready(function() {
-  $(".set > a").on("click", function() {
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-      $(this)
-        .siblings(".content")
-        .slideUp(200);
-      $(".set > a i")
-        .removeClass("fa-minus")
-        .addClass("fa-plus");
-    } else {
-      $(".set > a i")
-        .removeClass("fa-minus")
-        .addClass("fa-plus");
-      $(this)
-        .find("i")
-        .removeClass("fa-plus")
-        .addClass("fa-minus");
-      $(".set > a").removeClass("active");
-      $(this).addClass("active");
-      $(".content").slideUp(200);
-      $(this)
-        .siblings(".content")
-        .slideDown(200);
-    }
-  });
-});
-</script>
+

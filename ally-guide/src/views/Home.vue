0ccaf9188461a4ea7150656180b165efaa8ecc8c
Manisha Priyadarshini <manishapriya94@github.com>
2020-06-15 21:42:53 -0700

    Update Home.vue

--- ally-guide/src/views/Home.vue
+++ ally-guide/src/views/Home.vue
@@ -11,7 +11,7 @@
 </div>
 <div style = "background-color:#892819" class = "border-bottom border-white">
 <div class="container text-center">
-  <div class="box box1" ><a href = "/educate" class = "toggle">Educate </a></div>
+  <div class="box box1" ><a href = "/educate">Educate </a></div>
   <div class="box box2"> <a href = "/contribute"> Donate </a> </div>
   <div class="box box3"> <a href = "/elevate">Elevate  </a></div>
   <div class = "box box4"> <a href = "/collaborate">Collaborate </a></div>

febbc4092e997fa3c2114b3319b8bf55fb2f9bbc
Manisha <manisha.priya94@github.com>
2020-07-03 16:26:48 -0700

    Create vue.config.js

---
+++ ally-guide/vue.config.js
@@ -0,0 +1,12 @@
+cont path = require('path');
+
+module.exports = {
+  outputDir: path.resolve(__dirname, '../server/public'),
+  devServer:{
+    proxy: {
+      '/api':{
+        target: 'http://localhost:8080'
+      }
+    }
+  }
+};

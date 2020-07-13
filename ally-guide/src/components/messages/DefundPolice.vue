d6a7b78466a2840ddd59a300d8d6693ccd6c7333
BeeXiong <beexiong@github.com>
2020-07-12 18:37:06 -0700

    Elevate messages added

---
+++ ally-guide/src/components/messages/DefundPolice.vue
@@ -0,0 +1,36 @@
+<template>
+          <div class = "output">
+      <h1>To whom it may concern,
+</h1>
+      <br>
+      <p>Predatory policing, police brutality, and killing of black men, women, girls, and boys is not news. The video capture of George Floyd’s arrest and murder, along with the subsequent outrage and protests highlight the need for change in the criminal justice system.
+</p>
+<br>
+<p> Historically, procedural police reforms prove ineffective. The time to reverse decades of expansion of the scope, the intensity of, and the militarization of policing is now. Below are some petitions that support the cause.
+ </p>
+  <br>
+        <p> <a href = "https://www.change.org/p/national-action-against-police-brutality-and-murder"> National Action Against Police Brutality and Murder</a>, <a href = "https://act.colorofchange.org/sign/state-emergency-black-people-are-dying?source=coc_main_website">
+          End the War on Black People</a>, <a href = "https://peoplesbudgetla.com/">
+          LA City Council: Defund the Police</a>,
+          <a href = "https://secure.everyaction.com/eR7GA7oz70GL8doBq19LrA2"> Tell Minneapolis City Council to Defund the Police</a>.
+</p> <br>
+<p> Our ask is for acknowledgment and an end be put to the devaluation and dehumanization of black lives at the hand of police. This acknowledgment looks like the arrest and firing of officers who enforce law unethically, defunding of police, and reorientation of tax dollars to prison abolition efforts.
+        </p> <br>
+        <p>We ask instead, that social workers, health workers, and education have more funding to deescalate situations they are better trained for than police while ensuring children have the resources to avoid school to prison pipeline. We don’t need police in schools or community centers, we need social workers and higher volumes of them which can only be ensured by decreasing policing funds and prioritizing health, education, and community groups. Black lives matter, please continue to reflect this sentiment with real actions towards the end of police brutality instead of verbal consolations.
+</p> <br>
+        <p> Best, </p>
+       </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>

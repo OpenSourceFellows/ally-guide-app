3807751f54d571fca8e6611eebc29736855a1d86
Manisha <manisha.priya94@github.com>
2020-07-16 21:03:43 -0700

    Styling for collaborate, contribute, home

--- ally-guide/src/views/Collaborate.vue
+++ ally-guide/src/views/Collaborate.vue
@@ -1,32 +1,25 @@
 <template>
 <div>
-
-  <div class="rebuild-btn" style="padding-top= 400px;">
-    <a href = "https://www.pbs.org/newshour/nation/strike-for-black-lives-to-highlight-systemic-racism-ap-report-finds?fbclid=IwAR0tlpq1n2GphYy94t8kMOsW_RgiHSdmlY2Zlz2_ruNIT0DsavSDrT6IkxA"> Strike for Black Lives 7/20   </a>
-    <p> Walk out in solidarity or only spend at Black owned businesses. Workers are calling on companies to lift pay, permit the forming of unions and advocate for greater health care that can help narrow the economic gap that hinders Black and brown communities. </p>
+  <div style="padding-top: 300px;background-color: #ffbd59;">
+    <h1>Collaborate to keep up the momentum! </h1>
   </div>
-  <div>
-    lets see
+
+  <div style="background-color:#000000;">
+    <a href="https://j20strikeforblacklives.org/"> <img src= "https://bloximages.chicago2.vip.townnews.com/stlamerican.com/content/tncms/assets/v3/editorial/f/f1/ff1a210e-c54b-11ea-b238-1379e5cb8d4b/5f0ccbcb8387b.image.png?resize=500%2C500"></a>
   </div>
   <div class="rebuild-btn">
     <a href="https://airtable.com/shroExHXMjjio7J0y"> Add local demonstrations </a>
   </div>
-
-  <div class="rebuil-btn">
-    <a href="https://j20strikeforblacklives.org/"> <img src= "https://bloximages.chicago2.vip.townnews.com/stlamerican.com/content/tncms/assets/v3/editorial/f/f1/ff1a210e-c54b-11ea-b238-1379e5cb8d4b/5f0ccbcb8387b.image.png?resize=500%2C500"></a>
-  </div>
-
-<div class = "buyBlackOwned">
-  <a href = "https://officialblackwallstreet.com/app/"></a>
-  <h1> Buy Black Owned </h1>
+<div class = "rebuild-btn">
+  <a href = "https://officialblackwallstreet.com/app/">Buy Black Owned </a>
 </div>
 
-<div class = "BlackArtists">
+<div class = "rebuild-btn">
   <a href = "https://www.supportblackart.com/"> Support Black Artists </a>
 </div>
 
 <div class="rebuild-btn">
-<a href = "https://forms.gle/UNBGjouBDU1qNNJ26"></a>
+<a href = "https://forms.gle/UNBGjouBDU1qNNJ26">Join Ally.Guide</a>
 </div>
 </div>
 
@@ -45,17 +38,25 @@ export default {
 .rebuild-btn{
     background-color: #000000;
     text-decoration: none;
+    color:#ffffff;
     padding: 30px;
+    border: 5px solid #ffd594;
 }
 
 .rebuild-btn a{
   padding: 30px;
+  padding-left: 350px;
+  padding-right: 350px;
   width:100%;
+  color:#000000;
   color: white;
 }
 .rebuild-btn a:hover{
-  background-color:yellow;
+  background-color: #ffbd59;
   text-decoration:none;
+  color: #ffffff;
   color:#000000;
+
 }
+
 </style>

6d5cda2b8abf255838e8830421f9465e67d679e0
Manisha Priyadarshini <manishapriya94@github.com>
2020-08-04 13:09:34 -0700

    Update HeaderMobile.vue

--- ally-guide/src/components/partials/HeaderMobile.vue
+++ ally-guide/src/components/partials/HeaderMobile.vue
@@ -5,10 +5,10 @@
   <input class="menu-btn" type="checkbox" id="menu-btn" />
   <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
   <ul class="menu">
-    <li><a href="/Home">Home</a></li>
-    <li><a href="/Give">Give</a></li>
-    <li><a href="/Elevate">Amplify</a></li>
-    <li><a href="/Educate">Learn</a></li>
+    <li><a style="text-decoration:none; color:#000000;" href="/Home">Home</a></li>
+    <li><a style="text-decoration:none; color:#000000;" href="/Contribute">Give</a></li>
+    <li><a style="text-decoration:none; color:#000000;" href="/Elevate">Amplify</a></li>
+    <li><a style="text-decoration:none; color:#000000;" href="/Educate">Learn</a></li>
   </ul>
   </div>
 </header>

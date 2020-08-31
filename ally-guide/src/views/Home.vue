82b856ea5f38b7dfe58b4e2d810b954d853c6541
Mahrs <mahrsbar22@gmail.com>
2020-08-30 22:16:32 -0600

    update home page ui, update navbar

--- ally-guide/src/views/Home.vue
+++ ally-guide/src/views/Home.vue
@@ -1,31 +1,79 @@
 <template>
 	<div class="Home">
 		<div class="buffer"></div>
-		<div style="padding:30px;color:#fff; background-color:#262633">
-			 <h1> Ally Guide</h1>
-			 <p> Wherever you're starting from, we help you be consistent.</p>
+		<div class="topBanner">
+			<h1>Ally Guide</h1>Helping you move forward to becoming a consistent ally throughout your life.
 		</div>
-		<div>
+		<div class="outer">
 			<div class="action-image">
-				<img src="../assets/design-assets/Amplify.svg">
+				<img src="../assets/design-assets/Learn.svg" />
+			</div>
+			<div class="action-content">
+				<div class="arrow">
+					<img src="../assets/design-assets/next-arrow.svg" />
+				</div>
+				<div class="action-text learn">
+					<a href="/Educate">
+						<h2>Learn</h2>
+						<p class="info">Stay up to date on current narratives or take a learning path.</p>
+					</a>
+				</div>
 			</div>
 			<div class="action-image">
-				<img src="../assets/design-assets/Give.svg">
+				<img src="../assets/design-assets/Amplify.svg" />
 			</div>
-			<div class="action-image">
-				<img src="../assets/design-assets/Learn.svg">
-			</div>
-			<div class="action-text">
+			<div class="action-content">
+				<div class="arrow">
+					<img src="../assets/design-assets/next-arrow.svg" />
+				</div>
+				<div class="action-text amplify">
+					<a href="/Elevate">
+						<h2>Amplify</h2>
+						<p class="info">
+							Demand action from your representatives for
+							<b>Breonna Taylor, Vanessa Guillen, Lakota-Sioux to reclaim the Six Grandfathers</b> along with countless others who deal with state violence with ready-to-send emails.
+						</p>
 
+						<div class="twitter-buttons">
+							<button>
+								<a href="https://bit.ly/2BjsqBK">Justice for Breonna</a>
+							</button>
+							<button>
+								<a href="https://bit.ly/2CAp3Hi">Remove forces from Portland</a>
+							</button>
+							<button>
+								<a href="https://bit.ly/30tfUYQ">Justice for Vanessa</a>
+							</button>
+							<button>
+								<a href="https://bit.ly/2CTqRuM">Justice for Saraya</a>
+							</button>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="action-image">
+				<img src="../assets/design-assets/Give.svg" />
+			</div>
+			<div class="action-content">
+				<div class="arrow">
+					<img src="../assets/design-assets/next-arrow.svg" />
+				</div>
+				<div class="action-text give">
+					<a href="/Contribute">
+						<h2>Give</h2>
+						<p
+							class="info"
+						>Make an impact faster—contribute to local organizers and community funds in one click.</p>
+					</a>
+				</div>
 			</div>
 		</div>
-
 	</div>
 </template>
 
 <script>
 	export default {
 		name: "Home",
-		components: {}
+		components: {},
 	};
 </script>

a77a4e58a4403e9a6bd0ab3c8e254fc0aef8a45b
BeeXiong <beexiong64@gmail.com>
2020-09-17 11:16:58 -0600

    Update index.js
    test

--- server/index.js
+++ server/index.js
@@ -26,6 +26,6 @@ if(process.env.NODE_ENV === 'production'){
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;//test
 
 app.listen(port, () => console.log(`Server started on port ${port}`));

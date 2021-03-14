f722da1079856bd830788f7dc3f9f790ca676789
Travis Kopp <teakopp@github.com>
2021-03-14 16:24:53 -0700

    test returns response

--- server/__tests__/amplify.test.js
+++ server/__tests__/amplify.test.js
@@ -1,7 +1,6 @@
 const app = require("../index");
 const request = require("supertest");
 require("dotenv").config();
-
 afterEach(() => {
   jest.clearAllMocks();
 });
@@ -10,12 +9,12 @@ afterAll(async () => {
   await new Promise((resolve) => setTimeout(() => resolve(), 500)); // avoid jest open handle error
 });
 
-describe("/:zipCode", () => {
+describe("/api/amplify/:zipCode", () => {
   const zipCode = "92107"
-  const route = "/" + zipCode;
+  const route = "/api/amplify/" + zipCode;
   test("Returns congress members object ", async () => {
     const response = await request(app)
       .get(route)
-    console.log(response)
+    console.log(response.body)
   });
 })
